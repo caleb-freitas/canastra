a58e2a031a41438c1370afbfe2e2d9d190d679a1
caleb-freitas <calebfreitas@tutanota.com>
2022-09-06 10:27:39 -0300

    feat: change text color

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -17,10 +17,10 @@ const Home: NextPage = () => {
       </Head>
 
       <main className="container mx-auto flex flex-col items-center justify-center min-h-screen p-4">
-        <h1 className="text-5xl md:text-[5rem] leading-normal font-extrabold text-gray-700">
-          Create <span className="text-purple-300">T3</span> App
+        <h1 className="text-5xl md:text-[5rem] leading-normal font-extrabold">
+          Create <span className="text-purple-600">T3</span> App
         </h1>
-        <p className="text-2xl text-gray-700">This stack uses:</p>
+        <p className="text-2xl">This stack uses:</p>
         <div className="grid gap-3 pt-3 mt-3 text-center md:grid-cols-2 lg:w-2/3">
           <TechnologyCard
             name="NextJS"
@@ -55,8 +55,8 @@ const TechnologyCard = ({
 }: TechnologyCardProps) => {
   return (
     <section className="flex flex-col justify-center p-6 duration-500 border-2 border-gray-500 rounded shadow-xl motion-safe:hover:scale-105">
-      <h2 className="text-lg text-gray-700">{name}</h2>
-      <p className="text-sm text-gray-600">{description}</p>
+      <h2 className="text-lg">{name}</h2>
+      <p className="text-sm">{description}</p>
       <a
         className="mt-3 text-sm underline text-violet-500 decoration-dotted underline-offset-2"
         href={documentation}
